ff73adb88687afba17cd89bb25f64366ddbe6a3b
gukalex <guk.alex.v@gmail.com>
2022-07-20 17:43:09 +0200

    add parameterized url

--- main.js
+++ main.js
@@ -2,6 +2,27 @@ function randomIntFromInterval(min, max) { // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-randomNum = randomIntFromInterval(0, 3600);
-newUrl = "https://www.youtube.com/embed/EColTNIbOko?autoplay=1&start=" + randomNum; 
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
+
+videoId = findGetParameter('videoId');
+type = findGetParameter('type');
+start = findGetParameter('start');
+end = findGetParameter('end');
+
+// type 0 - regular video
+randomNum = randomIntFromInterval(parseInt(start), parseInt(end));
+newUrl = 'https://www.youtube.com/embed/' +  videoId + '?autoplay=1&start=' + randomNum;
+newUrl += videoId
 window.location.replace(newUrl);
+// todo: type 1 - playlist
