2f4a6752b3c9f829b40d4a45c4a07cc2bb15562d
gukalex <guk.alex.v@gmail.com>
2022-07-22 16:20:41 +0200

    fix indentation and extra bracket

--- main.js
+++ main.js
@@ -668,7 +668,7 @@ f`
   div.innerHTML += '<p>' + message + '</p>';
 }
 if ('github' == type) {
-  function init(){
+  function init() {
     var number = getRandomIntInclusive(0, 60000000);
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://api.github.com/repositories?since=' + number, true);
@@ -676,18 +676,17 @@ if ('github' == type) {
     xhr.onreadystatechange = function(e){
       openRepo(e, xhr)
     };
-	}
-	function getRandomIntInclusive(min, max) {
-		min = Math.ceil(min);
-		max = Math.floor(max);
-		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
-	function openRepo (e, xhr){
-		if (xhr.readyState == 4 && xhr.status == 200) {
-			var full_name = JSON.parse(xhr.responseText)[0]['full_name'];
-			location.href = 'https://github.com/' + full_name;
-		}
-	}
-	init();
-	} 
+  }
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  function openRepo (e, xhr) {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var full_name = JSON.parse(xhr.responseText)[0]['full_name'];
+      location.href = 'https://github.com/' + full_name;
+    }
+  }
+  init();
 }
